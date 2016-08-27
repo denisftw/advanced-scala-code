bd75fc3b9319469eefa8db8462f4cd264a0988cf
denny2006 <denny2006@gmail.com>
2016-08-27 18:30:17 +0300

    Generic codec derivation example - the final version

--- shapeless/src/main/scala/CodecDerivation.scala
+++ shapeless/src/main/scala/CodecDerivation.scala
@@ -1,4 +1,3 @@
-import CodecDerivation.JsonEncoder.addQuotes
 import shapeless.ops.hlist.{Mapper, ToTraversable}
 import shapeless._
 import shapeless.ops.record.{Keys, Values}
@@ -9,21 +8,25 @@ import shapeless.ops.record.{Keys, Values}
 object CodecDerivation {
 
   object JsonEncoder {
-    object addQuotes extends Poly1 {
+
+    object quoteStrings extends Poly1 {
       implicit def str2str = at[String](str => s""""$str"""")
       implicit def default[A] = at[A](identity)
     }
-    def doEncode[T <: HList, R <: HList, K <: HList, V <: HList](s: T)(
-      implicit keyExtractor: Keys.Aux[T, K],
+
+    def doEncode[A, T <: HList, K <: HList, V <: HList, R <: HList](a: A)(
+      implicit gen: LabelledGeneric.Aux[A, T],
+      keyExtractor: Keys.Aux[T, K],
       valueExtractor: Values.Aux[T, V],
-      valueMapper: Mapper.Aux[addQuotes.type, V, R],
+      valueMapper: Mapper.Aux[quoteStrings.type, V, R],
       toTraversableValues: ToTraversable.Aux[R, List, Any],
       toTraversableKeys: ToTraversable.Aux[K, List, Symbol]
-        ): String = {
+    ): String = {
+      val repr = gen.to(a)
       val keys = keyExtractor.apply
-      val values = valueExtractor.apply(s)
+      val values = valueExtractor.apply(repr)
 
-      val quoted = values.map(addQuotes)
+      val quoted = values.map(quoteStrings)
       val zipped = keys.toList.zip(quoted.toList)
 
       val elements = zipped.map { case (label, value) =>
@@ -32,21 +35,15 @@ object CodecDerivation {
       val json = elements.mkString("{", ",", "}")
       json
     }
-
-    def encode[A, T <: HList](a: A)(implicit gen: LabelledGeneric.Aux[A, T], keys: Keys[T]): T = {
-      val repr = gen.to(a)
-      repr
-    }
   }
 
   def main(args: Array[String]) {
 
     case class Person(firstName: String, lastName: String, age: Int)
-
     val person = Person("Joe", "Black", 42)
-    val repr = JsonEncoder.encode(person)
-    val json = JsonEncoder.doEncode(repr)
 
+    val json = JsonEncoder.doEncode(person)
     println(json)
+    // prints {"firstName":"Joe","lastName":"Black","age":42}
   }
 }
