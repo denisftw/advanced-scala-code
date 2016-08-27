2cbdb915e920d87cb18e6793cf969db8582f7b56
denny2006 <denny2006@gmail.com>
2016-08-27 13:59:21 +0300

    Generic codec derivation example - almost correct

--- build.sbt
+++ build.sbt
@@ -77,7 +77,7 @@ val monocleDependencies = Seq(
 )
 
 val shapelessDependencies = Seq(
-  "com.chuusai" %% "shapeless" % "2.3.2"
+  "com.chuusai" %% "shapeless" % "2.3.1"
 )
 
 
--- shapeless/src/main/scala/CodecDerivation.scala
+++ shapeless/src/main/scala/CodecDerivation.scala
@@ -1,5 +1,7 @@
+import CodecDerivation.JsonEncoder.addQuotes
 import shapeless.ops.hlist.{Mapper, ToTraversable}
 import shapeless._
+import shapeless.ops.record.{Keys, Values}
 
 /**
   * Created by denis on 8/26/16.
@@ -8,18 +10,33 @@ object CodecDerivation {
 
   object JsonEncoder {
     object addQuotes extends Poly1 {
-      implicit def str2str = at[String](str => s""""$str"""")
+      implicit def str2str = at[String](_.toUpperCase)
       implicit def default[A] = at[A](identity)
     }
+    def doEncode[T <: HList, R <: HList, K <: HList, V <: HList](s: T)(
+      implicit keys: Keys.Aux[T, K],
+      values: Values.Aux[T, V],
+      mapper: Mapper.Aux[addQuotes.type, T, R],
+      toTraversableValues: ToTraversable.Aux[R, List, Any],
+      toTraversableKeys: ToTraversable.Aux[K, List, Symbol]
+        ): String = {
+      val keyz = keys.apply
+      val valuez = values.apply(s)
 
-    def doEncode[T <: HList, R <: HList](s: T)(
-        implicit mapper: Mapper.Aux[addQuotes.type, T, R],
-        toTraversable: ToTraversable.Aux[R, List, Any]): String = {
       val im = s.map(addQuotes)
-      im.mkString("{", ",", "}")
+
+      val keysList = keyz.toList
+      val valsList = im.toList
+      val zipped = keysList.zip(valsList)
+
+      val elements = zipped.map { case (label, value) =>
+        "\"" + label.name + "\"" + ":" + value.toString
+      }
+      val json = elements.mkString("{", ",", "}")
+      json
     }
 
-    def encode[A, T <: HList](a: A)(implicit gen: Generic.Aux[A, T]): T = {
+    def encode[A, T <: HList](a: A)(implicit gen: LabelledGeneric.Aux[A, T], keys: Keys[T]): T = {
       val repr = gen.to(a)
       repr
     }
@@ -28,7 +45,6 @@ object CodecDerivation {
   def main(args: Array[String]) {
 
     case class Person(firstName: String, lastName: String, age: Int)
-    implicit val gen = Generic[Person]
 
     val person = Person("Joe", "Black", 42)
     val repr = JsonEncoder.encode(person)
