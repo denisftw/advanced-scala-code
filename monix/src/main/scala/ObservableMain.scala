fd8036525175854c104caa999f8d8ba876482783
denny2006 <denny2006@gmail.com>
2016-09-19 23:51:17 +0300

    The Loan pattern is unnecessary with Observables and doesn't work there anyway

--- monix/src/main/scala/ObservableMain.scala
+++ monix/src/main/scala/ObservableMain.scala
@@ -7,37 +7,34 @@ object ObservableMain {
     import java.io.{BufferedReader, FileReader}
     val br = new BufferedReader(new FileReader("license.txt"))
 
-    arm.ArmUtils.using(br) {
-      import monix.reactive.Observable
-      val linesO = Observable.fromLinesReader(br)
-      val wordsO = linesO.flatMap { line =>
-        val arr = line.split("\\W").map(_.toLowerCase)
-          .map(_.trim).filter(!_.isEmpty)
-        Observable.fromIterable(arr.toIterable)
-      }
-
-      val rawResultO = wordsO.foldLeftF(Map.empty[String, Int]) { (acc, next) =>
-        acc.get(next) match {
-          case None => acc + (next -> 1)
-          case Some(num) => acc + (next -> (1 + num))
-        }
-      }
-
-      import monix.reactive.Consumer
-      val finalResultT = rawResultO.map { map =>
-        map.toList.sortWith( _._2 > _._2).take(5).map(_._1)
-      }.runWith(Consumer.head)
-
-      import monix.execution.Scheduler.Implicits.global
-      val resultCF = finalResultT.runAsync
-
-      import scala.concurrent.Await
-      import scala.concurrent.duration._
-      val result = Await.result(resultCF, 30.seconds)
-      println(result)
-      // List(the, or, of, and, to)
-
+    import monix.reactive.Observable
+    val linesO = Observable.fromLinesReader(br)
+    val wordsO = linesO.flatMap { line =>
+      val arr = line.split("\\W").map(_.toLowerCase)
+        .map(_.trim).filter(!_.isEmpty)
+      Observable.fromIterable(arr.toIterable)
     }
+
+    val rawResultO = wordsO.foldLeftF(Map.empty[String, Int]) { (acc, next) =>
+      acc.get(next) match {
+        case None => acc + (next -> 1)
+        case Some(num) => acc + (next -> (1 + num))
+      }
+    }
+
+    import monix.reactive.Consumer
+    val finalResultT = rawResultO.map { map =>
+      map.toList.sortWith( _._2 > _._2).take(5).map(_._1)
+    }.runWith(Consumer.head)
+
+    import monix.execution.Scheduler.Implicits.global
+    val resultCF = finalResultT.runAsync
+
+    import scala.concurrent.Await
+    import scala.concurrent.duration._
+    val result = Await.result(resultCF, 30.seconds)
+    println(result)
+    // List(the, or, of, and, to)
   }
 
   import cats.kernel.Monoid
