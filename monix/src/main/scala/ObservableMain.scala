5ad5f78f3e150bbc39d4fa87aafe57d57cc5f850
denny2006 <denny2006@gmail.com>
2016-09-21 20:42:27 +0300

    Moving the trampolining example to the Cats subproject and making an observable restartable

--- monix/src/main/scala/ObservableMain.scala
+++ monix/src/main/scala/ObservableMain.scala
@@ -1,40 +1,51 @@
+import monix.reactive.Observable
+
 /**
   * Created by denis on 9/1/16.
   */
 object ObservableMain {
 
   def main(args: Array[String]): Unit = {
-    import java.io.{BufferedReader, FileReader}
-    val br = new BufferedReader(new FileReader("license.txt"))
 
     import monix.reactive.Observable
-    val linesO = Observable.fromLinesReader(br)
-    val wordsO = linesO.flatMap { line =>
-      val arr = line.split("\\W").map(_.toLowerCase)
-        .map(_.trim).filter(!_.isEmpty)
-      Observable.fromIterable(arr.toIterable)
+    val linesO = Observable.defer {
+      import java.io.{BufferedReader, FileReader}
+      val br = new BufferedReader(new FileReader("license.txt"))
+      Observable.fromLinesReader(br)
     }
 
-    val rawResultO = wordsO.foldLeftF(Map.empty[String, Int]) { (acc, next) =>
-      acc.get(next) match {
-        case None => acc + (next -> 1)
-        case Some(num) => acc + (next -> (1 + num))
+    printStatistics(linesO)
+    printStatistics(linesO)
+
+    def printStatistics(linesO: Observable[String]) = {
+      val wordsO = linesO.flatMap { line =>
+        val arr = line.split("\\W").map(_.toLowerCase)
+          .map(_.trim).filter(!_.isEmpty)
+        Observable.fromIterable(arr.toIterable)
       }
+
+      val rawResultO = wordsO.foldLeftF(Map.empty[String, Int]) { (acc, next) =>
+        acc.get(next) match {
+          case None => acc + (next -> 1)
+          case Some(num) => acc + (next -> (1 + num))
+        }
+      }
+
+      import monix.reactive.Consumer
+      val finalResultT = rawResultO.map { map =>
+        map.toList.sortWith( _._2 > _._2).take(5).map(_._1)
+      }.runWith(Consumer.head)
+
+      import monix.execution.Scheduler.Implicits.global
+      val resultCF = finalResultT.runAsync
+
+      import scala.concurrent.Await
+      import scala.concurrent.duration._
+      val result = Await.result(resultCF, 30.seconds)
+      println(result)
+      // List(the, or, of, and, to)
     }
 
-    import monix.reactive.Consumer
-    val finalResultT = rawResultO.map { map =>
-      map.toList.sortWith( _._2 > _._2).take(5).map(_._1)
-    }.runWith(Consumer.head)
-
-    import monix.execution.Scheduler.Implicits.global
-    val resultCF = finalResultT.runAsync
-
-    import scala.concurrent.Await
-    import scala.concurrent.duration._
-    val result = Await.result(resultCF, 30.seconds)
-    println(result)
-    // List(the, or, of, and, to)
   }
 
   import cats.kernel.Monoid
