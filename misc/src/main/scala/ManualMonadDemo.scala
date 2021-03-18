6f3fa73a24fb297092b13cc3f493d140253eda24
denisftw <denny2006@gmail.com>
2021-03-18 20:36:54 +1100

    Finishing the monad implementation for Optional

--- misc/src/main/scala/ManualMonadDemo.scala
+++ misc/src/main/scala/ManualMonadDemo.scala
@@ -8,21 +8,38 @@ object MonadOps {
     def flatMap[B](f: A => F[B]): F[B] = monadEv.flatMap(fa)(f)
   }
 }
-trait Optional[+A]
-case class Presence[A](a: A) extends Optional[A]
-case object Absence extends Optional[Nothing]
+trait Optional[+A] {
+  def extract: A
+  def transform[B](f: A => B): Optional[B]
+  def bind[B](f: A => Optional[B]): Optional[B]
+}
+case class Presence[A](a: A) extends Optional[A] {
+  override def transform[B](f: A => B): Optional[B] = Presence(f(a))
+  override def bind[B](f: A => Optional[B]): Optional[B] = f(a)
+  override def extract: A = a
+}
+case object Absence extends Optional[Nothing] {
+  override def transform[B](f: Nothing => B): Optional[B] = Absence
+  override def bind[B](f: Nothing => Optional[B]): Optional[B] = Absence
+  override def extract: Nothing = throw new NullPointerException
+}
 
 object Optional {
-  // TODO: Add Monad[Optional]
   implicit val monadInstance: Monad[Optional] = new Monad[Optional] {
-    override def map[A, B](fa: Optional[A])(f: A => B): Optional[B] = {}
-    override def flatMap[A, B](fa: Optional[A])(f: A => Optional[B]): Optional[B] = {}
+    override def map[A, B](fa: Optional[A])(f: A => B): Optional[B] = {
+      fa.transform(f)
+    }
+    override def flatMap[A, B](fa: Optional[A])(f: A => Optional[B]): Optional[B] = {
+      fa.bind(f)
+    }
   }
 }
 
 object ManualMonadDemo {
-  def sumIntOptionals(oa: Optional[Int], ob: Optional[Int])(implicit monadEv: Monad[Optional]): Optional[Int] = {
+//  def sumIntOptionals(oa: Optional[Int], ob: Optional[Int])(implicit monadEv: Monad[Optional]): Optional[Int] = {
+  def sumIntOptionals[Optional[_]: Monad](oa: Optional[Int], ob: Optional[Int]): Optional[Int] = {
     // 1. desugared
+    val monadEv = implicitly[Monad[Optional]]
     monadEv.flatMap(oa) { a =>
       monadEv.map(ob) { b =>
         a + b
@@ -39,6 +56,6 @@ object ManualMonadDemo {
   def main(args: Array[String]): Unit = {
     val oa: Optional[Int] = Presence(23)
     val ob: Optional[Int] = Presence(27)
-    sumIntOptionals(oa, ob)
+    println(sumIntOptionals(oa, ob).extract)
   }
 }
